58e0297dac5f6625fc32a67c7afde8b2e9018018
Matt Martin <matthewjmartin@zoho.com>
2021-04-29 13:20:20 +0100

    Created layout, header & footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,24 +1,26 @@
 import Head from 'next/head'
+import Header from './Header'
+import Footer from './Footer'
 import styles from '../styles/Layout.module.css'
 
-export default function Layout({title, keywords, description, children}) {
-    return (
-        <div>
-            <Head>
-              <title>{title}</title>
-              <meta name='description' content={description} />
-              <meta name='keywords' content={keywords} />
-            </Head>
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+      </Head>
 
-            <div className={styles.container}>
-                {children}
-            </div>
-        </div>
-    )
+      <Header />
+      <Footer />
+      <div className={styles.container}>{children}</div>
+    </div>
+  )
 }
 
 Layout.defaultProps = {
-    title: 'Music Events | Find the hottest parties',
-    description: 'Find the latest music and other events',
-    keywords: 'music, dj, band, electronic, events'
+  title: 'Music Events | Find the hottest parties',
+  description: 'Find the latest music and other events',
+  keywords: 'music, dj, band, electronic, events',
 }
